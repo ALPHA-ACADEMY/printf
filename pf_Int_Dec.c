fc23eca13a53027646122f70458282b4cd95c633
Abderrahim with Loubna <benmhandcreative@gmail.com>
2023-09-18 08:06:44 -0700

    Hot fix

--- pf_Int_Dec.c
+++ pf_Int_Dec.c
@@ -10,34 +10,43 @@ int pf_Int_Dec(va_list args)
 {
 	int n = va_arg(args, int);
 	int num;
-	int i = 0;
+	int i = 1;
 	int digit;
 	int exp = 1;
+	int last_digit = n % 10;
 
-	if (n < 0)
+	n /= 10;
+	num = n;
+
+	if (last_digit < 0)
 	{
 		_putchr('-');
+		num = -num;
 		n = -n;
+		last_digit = -last_digit;
 		i++;
 	}
 
-	num = n;
-
-	while (num / 10 > 0)
+	if (num > 0)
 	{
-		exp *= 10;
-		num /= 10;
-	}
 
-	num = n;
-	while (exp > 0)
-	{
-		digit = num / exp;
-		_putchr(digit + '0');
-		num -= digit * exp;
-		exp /= 10;
-		i++;
+		while (num / 10 != 0)
+		{
+			exp *= 10;
+			num /= 10;
+		}
+
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchr(digit + '0');
+			num -= digit * exp;
+			exp /= 10;
+			i++;
+		}
 	}
+	_putchr(last_digit + '0');
 
 
 	return (i);
